4e8af44ccfbce133f0479807e3d4988f5946f95e
legianha <doantrihung592@gmail.com>
2021-01-14 19:00:59 +0700

    add blog

---
+++ jpa/blog/src/main/java/vn/techmaster/blog/controller/PostController.java
@@ -0,0 +1,19 @@
+package vn.techmaster.blog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller
+public class PostController {
+  @GetMapping("/posts")
+  public String getAllPosts() {
+    return "posts.html";
+  }
+}
